fab13a6a1b732908c438792fc20dcdbf81650ee3
jamesmcarthur-3999 <jamesmcarthur3999@gmail.com>
2025-04-21 16:23:56 -0700

    Update environment variable generator to handle REACT_APP_ prefixed variables

--- scripts/generate-env.js
+++ scripts/generate-env.js
@@ -5,11 +5,16 @@
 
 const fs = require('fs');
 const path = require('path');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
 
 // List of environment variables that should be exposed to the browser
 const ALLOWED_ENV_VARS = [
   'NODE_ENV',
-  'APP_PORT',
+  'PORT',
+  'REACT_APP_API_URL',
   'ENABLE_CACHING', 
   'DEFAULT_GRID_LAYOUT',
   'MAX_CARDS_PER_VIEW',
@@ -31,9 +36,16 @@ const generateConfig = () => {
       }
     });
     
+    // Add all REACT_APP_* environment variables
+    Object.keys(process.env).forEach(key => {
+      if (key.startsWith('REACT_APP_')) {
+        publicEnvObj[key] = process.env[key];
+      }
+    });
+    
     // Default values for required variables
     if (!publicEnvObj.NODE_ENV) publicEnvObj.NODE_ENV = 'development';
-    if (!publicEnvObj.APP_PORT) publicEnvObj.APP_PORT = '3000';
+    if (!publicEnvObj.PORT) publicEnvObj.PORT = '3000';
     if (!publicEnvObj.ENABLE_CACHING) publicEnvObj.ENABLE_CACHING = 'true';
     if (!publicEnvObj.MAX_CARDS_PER_VIEW) publicEnvObj.MAX_CARDS_PER_VIEW = '12';
     
@@ -51,7 +63,7 @@ const generateConfig = () => {
     fs.writeFileSync(configPath, configContent);
     
     console.log(`Environment configuration generated at ${configPath}`);
-    console.log(`Exposed variables: ${ALLOWED_ENV_VARS.join(', ')}`);
+    console.log(`Exposed variables: ${Object.keys(publicEnvObj).join(', ')}`);
     
     return true;
   } catch (error) {
